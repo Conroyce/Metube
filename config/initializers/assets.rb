00e89b0e41f9dcda28a709765ebe710350439fd4
Connor Kojimoto <cokojimoto@gmail.com>
2014-12-22 10:04:39 -0800

    added asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( videos.css )
+Rails.application.config.assets.precompile += %w( videos.js )
+
+Rails.application.config.assets.precompile += %w( playlists.css )
+Rails.application.config.assets.precompile += %w( playlists.js )
+
+Rails.application.config.assets.precompile += %w( comments.css )
+Rails.application.config.assets.precompile += %w( comments.js )
+
